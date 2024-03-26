d6cc26f09f1265b1c18919e580914f8ac8a472c7
DeniseEngert <10726000+DeniseEngert@users.noreply.github.com>
2024-03-26 20:41:19 +0100

    add dockerized broker

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Playing around with MQTT, nothing sophisticated
+
+## Start Broker
+    docker run -it -p 1883:1883 -p 9001:9001 -v <path to>/mosquitto.conf:/mosquitto/config/mosquitto.conf <container name>
--- mqtt_publisher/publisher.py
+++ mqtt_publisher/publisher.py
@@ -1,12 +1,14 @@
 import paho.mqtt.client as mqtt
 
-mqttBroker = "mqtt.eclipseprojects.io"
+#mqttBroker = "mqtt.eclipseprojects.io"
+mqttBroker = "127.0.0.1"
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.connect(mqttBroker)
 
 
 def send_out(topic, temp):
-    client.publish(topic, temp)
+    ret = client.publish(topic, temp)
+    print("Return: ", ret[0], "   ", ret[1])
     print("Just published ", str(temp) + " to topic ", topic)
     print("In wait loop")
